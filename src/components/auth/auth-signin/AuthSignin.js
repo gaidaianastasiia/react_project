44680ecc3f2aae10c58bd3eb6c842ae868cff173
gaidaianastasiia <haidaianastasiia0102@meta.ua>
2020-01-25 10:53:55 +0200

    create auth part

--- src/components/auth/auth-signin/AuthSignin.js
+++ src/components/auth/auth-signin/AuthSignin.js
@@ -1,19 +1,31 @@
 import React, {Component} from 'react';
 import "./AuthSignin.css";
+import "../Auth.css";
 import Input from "../../common/input/Input";
+import Button from "../../common/button/Button";
+import Loader from "../../common/loader/Loader";
+import {Link, Redirect} from "react-router-dom";
+import AuthService from "../../../services/AuthService";
+import ValidationService from "../../../services/ValidationService";
+import {INTERNAL_SERVER_ERROR, AUTH_SERVER_ERR_MESSAGES} from "../../../constants/apiErrMessages";
 
 export default class AuthSignin extends Component {
+    constructor() {
+        super();
+        this.authService = new AuthService();
+        this.validationService = new ValidationService();
+    }
+
     state = {
         user: {
-            email: {
-                value: "",
-                errMessage: "",
-            },
-            password: {
-                value: "",
-                errMessage: "",
-            }
-        }
+            email: "",
+            password: ""
+        },
+        emailErrMessage: "",
+        passErrMessage: "",
+        serverErrMessage: "",
+        showLoader: false,
+        isRedirect: false
     }
 
     handleInputChange = ({target: {name, value}}) => {
@@ -21,55 +33,105 @@ export default class AuthSignin extends Component {
             ...this.state,
             user: {
                 ...this.state.user,
-                email: {
-                    ...this.state.user.email,
-                    [name]: value
-                }
+                [name]: value
             }
         });
     }
 
-    renderInputs = (inputsData) => {
-        return Object.keys(inputsData)
-            .map(control => {
-                console.log(control)
+    handleSubmit = () => {
+        this.clearErrMessages();
+
+        const {email, password} = this.state.user;
+        const emailValidData = this.validationService.validateEmail(email);
+        const passwordValidData = this.validationService.validatePassword(password);
+
+        if (emailValidData.isValid && passwordValidData.isValid) {
+            this.signin({email, password});
+        } else {
+            this.setState({
+                ...this.state,
+                emailErrMessage: emailValidData.errMessage,
+                passErrMessage: passwordValidData.errMessage
+            });
+        }
+    }
+
+    clearErrMessages = () => {
+        this.setState({
+            ...this.state,
+            emailErrMessage: "",
+            passErrMessage: "",
+            serverErrMessage: ""
+        });
+    }
+
+    signin = user => {
+        this.setLoaderState(true);
+
+        this.authService.signin(user)
+            .then(() => {
+                this.setLoaderState(false);
+                this.setRedirectState(true);
+            })
+            .catch(err => {
+                this.setLoaderState(false);
+                this.showServerErrMessage(err);
             });
     }
 
+    setLoaderState = (state) => {
+        this.setState({
+            ...this.state,
+            showLoader: state
+        });
+    }
+
+    setRedirectState = state => {
+        this.setState({
+            ...this.state,
+            isRedirect: state
+        });
+    }
+
+    setServerResponseResult = () => {
+        this.setState({
+            ...this.state,
+            isServerResponseSuccessful: true
+        });
+    }
+
+    showServerErrMessage = err => {
+        switch (err) {
+            case 500:
+                this.setServerErrMessage(INTERNAL_SERVER_ERROR);
+                break;
+            case 602:
+                this.setServerErrMessage(AUTH_SERVER_ERR_MESSAGES.WRONG_EMAIL_OR_PASSWORD);
+                break;
+            default:
+        }
+    }
+
+    setServerErrMessage = errMessage => {
+        this.setState({
+            ...this.state,
+            serverErrMessage: errMessage
+        });
+    }
+
     render() {
-        const {value: emailValue, errMessage: emailErrMessage} = this.state.user.email;
-        const {value: passValue, errMessage: passErrMessage} = this.state.user.password;
-
-        const inputsData = {
-            email: {
-                type: "email",
-                name: "email",
-                value: emailValue,
-                onChange: this.handleInputChange,
-                labelText: "Email",
-                errorMessage: emailErrMessage
-            },
-            password: {
-                type: "password",
-                name: "emapasswordil",
-                value: passValue,
-                onChange: this.handleInputChange,
-                labelText: "Password",
-                errorMessage: passErrMessage
-            }
-        };
-
-        console.log(this.renderInputs(inputsData));
+        const {email, password} = this.state.user;
+        const {emailErrMessage, passErrMessage, serverErrMessage, showLoader, isRedirect} = this.state;
 
         return (
             <section className="auth">
-                <h2 className="auth__title">Signin</h2>
-
                 <form action="#" className="auth__form">
+                    <h2 className="auth__title">Sign in</h2>
+                    <p className="auth__server-err-message">{serverErrMessage}</p>
                     <Input
                         type={"email"}
                         name={"email"}
-                        value={emailValue}
+                        value={email}
                         onChange={this.handleInputChange}
                         labelText={"Email"}
                         errorMessage={emailErrMessage}
@@ -77,12 +139,19 @@ export default class AuthSignin extends Component {
                     <Input
                         type={"password"}
                         name={"password"}
-                        value={passValue}
+                        value={password}
                         onChange={this.handleInputChange}
                         labelText={"Password"}
                         errorMessage={passErrMessage}
                     />
+                    <Button onClick={this.handleSubmit}>Signin</Button>
                 </form>
+                <div className="auth__link">
+                    <Link to="/signup">No account? Create one here.</Link>
+                </div>
+
+                {showLoader && <Loader/>}
+                {isRedirect && <Redirect to={{pathname: '/news'}}/>}
             </section>
         )
     }
---
+++ src/services/LocalTokenService.js
@@ -0,0 +1,18 @@
+const TOKEN_KEY = "TOKEN_KEY";
+
+export default class LocalTokenService {
+
+    saveToken = token => {
+        localStorage.setItem(TOKEN_KEY, token);
+    };
+
+    //метод вернет вам токен текущего пользователя,
+    //который вы будете передавть как аргумент в методах своих сервисов
+    getToken = () => {
+        return localStorage.getItem(TOKEN_KEY);
+    };
+
+    removeToken = () => {
+        localStorage.removeItem(TOKEN_KEY);
+    }
+}
