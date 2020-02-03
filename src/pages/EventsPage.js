de272da6ab0c5696be04d8f76d88c7403d273d3c
gaidaianastasiia <haidaianastasiia0102@meta.ua>
2020-02-03 12:16:34 +0200

    refactoring

--- src/pages/EventsPage.js
+++ src/pages/EventsPage.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import BaseTemplate from "../../components/base-template/BaseTemplate";
-import Events from "../../components/events/Events";
+import BaseTemplate from "../components/base-template/BaseTemplate";
+import Events from "../components/events/Events";
 
 export default class EventsPage extends Component {
     render() {
--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,7 +1,7 @@
 import React, {Component, Fragment} from 'react';
-import Header from "../../components/common/header/Header";
-import Auth from "../../components/auth/Auth";
-import Footer from "../../components/common/footer/Footer";
+import Header from "../components/common/header/Header";
+import Auth from "../components/auth/Auth";
+import Footer from "../components/common/footer/Footer";
 
 export default class SignupPage extends Component {
     render() {
